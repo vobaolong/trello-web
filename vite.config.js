32385a06cb05eeb7e0818eae9d28fd81c164a9c3
Võ Bảo Long <75128770+vobaolong@users.noreply.github.com>
2023-08-22 14:48:04 +0700

    Relative Absolute Import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // resolve: {
+  //   alias: [{ find: '~', replacement: '/src' }]
+  // }
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      }
+    ]
+  }
 })
