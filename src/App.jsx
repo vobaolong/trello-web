1f0e73daf81bd71969ea58968b5ca13b1b91538c
Võ Bảo Long <75128770+vobaolong@users.noreply.github.com>
2023-08-22 12:21:41 +0700

    feat: change CssVarsProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,55 @@
 import './App.css'
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material'
 import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
+      <InputLabel id='label-select-dark-light'>Mode</InputLabel>
+      <Select
+        labelId='label-select-dark-light'
+        id='demo-select-small'
+        value={mode}
+        label='Mode'
+        onChange={handleChange}
+      >
+        <MenuItem value='light'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize='small' /> Light
+          </Box>
+        </MenuItem>
+        <MenuItem value='dark'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeOutlinedIcon fontSize='small' /> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value='system'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsBrightnessIcon fontSize='small' /> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
 function App() {
   return (
     <>
-      <Typography component='div'>
-        <Box sx={{ textTransform: 'uppercase', m: 1 }}>HEllo</Box>
-      </Typography>
-      <Button variant='outlined'>Hello world</Button>
-      <Button variant='contained'>Hello world</Button>
-      <AccessAlarmIcon />
+      <ModeSelect />
     </>
   )
 }
