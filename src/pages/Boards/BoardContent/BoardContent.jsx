907dfc8554ab7041ab23a025b3d6820dd9390651
Võ Bảo Long <75128770+vobaolong@users.noreply.github.com>
2023-08-23 21:41:39 +0700

    feat: add dnd-toolkit

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,11 +1,60 @@
+import { useEffect, useState } from 'react'
 import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '@/utils/sorts'
+import {
+  DndContext,
+  // PointerSensor,
+  useSensor,
+  useSensors,
+  MouseSensor,
+  TouchSensor
+} from '@dnd-kit/core'
+import { arrayMove } from '@dnd-kit/sortable'
 
 function BoardContent({ board }) {
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  // di chuyển chuột 10px thì mới kích hoạt
+  // const pointerSensor = useSensor(PointerSensor, {
+  //   activationConstraint: { distance: 10 }
+  // })
+  // Di chuyển chuột 10px thì mới kích hoạt event
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: { distance: 10 }
+  })
+  // Nhấn giữ 250ms (1/4s) và di chuyển 500px thì mới kích hoạt event
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: { delay: 250, tolerance: 500 }
+  })
+  // const sensors = useSensors(pointerSensor)
+  const sensors = useSensors(mouseSensor, touchSensor)
+
+  const [orderedColumns, setOrderedColumns] = useState([])
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+  const handleDragEnd = (event) => {
+    // console.log(event)
+    const { active, over } = event
+    // kiểm tra nếu không tồn tại over thì return tránh lỗi
+    if (!over) return
+    if (active.id != over.id) {
+      // lấy vị trí cũ
+      const oldIndex = orderedColumns.findIndex(
+        (column) => column._id === active.id
+      )
+      // lấy vị trí mới
+      const newIndex = orderedColumns.findIndex(
+        (column) => column._id === over.id
+      )
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      // const dndOrderedColumnsIds = dndOrderedColumns.map((column) => column._id)
+      // console.log('dndOrderedColumns', dndOrderedColumns)
+      // console.log('dndOrderedColumnsIds', dndOrderedColumnsIds)
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
   return (
-    <>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
       <Box
         sx={{
           bgcolor: (theme) =>
@@ -17,7 +66,7 @@ function BoardContent({ board }) {
       >
         <ListColumns columns={orderedColumns} />
       </Box>
-    </>
+    </DndContext>
   )
 }
 
