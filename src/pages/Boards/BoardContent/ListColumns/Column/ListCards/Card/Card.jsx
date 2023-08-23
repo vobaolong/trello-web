7aa35d0f396538d8b836391843b80279f66d831c
Võ Bảo Long <75128770+vobaolong@users.noreply.github.com>
2023-08-23 12:59:28 +0700

    feat: Board Content & Refactor Struction

---
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -0,0 +1,60 @@
+import { Card as MuiCard } from '@mui/material'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import GroupIcon from '@mui/icons-material/Group'
+import CommentIcon from '@mui/icons-material/Comment'
+import AttachmentIcon from '@mui/icons-material/Attachment'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+
+function Card({ temporaryHideMedia }) {
+  if (temporaryHideMedia) {
+    return (
+      <MuiCard
+        sx={{
+          cursor: 'pointer',
+          boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+          overflow: 'unset'
+        }}
+      >
+        <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
+          <Typography>Task No Media</Typography>
+        </CardContent>
+      </MuiCard>
+    )
+  }
+  return (
+    <>
+      <MuiCard
+        sx={{
+          cursor: 'pointer',
+          boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
+          overflow: 'unset'
+        }}
+      >
+        <CardMedia
+          sx={{ height: 140 }}
+          image='https://dokodemo-kerja.com/blog/wp-content/uploads/2021/09/task-management.jpg'
+          title='green iguana'
+        />
+        <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
+          <Typography>Task 1</Typography>
+        </CardContent>
+        <CardActions sx={{ p: '0 4px 8px 4px' }}>
+          <Button size='small' startIcon={<GroupIcon />}>
+            20
+          </Button>
+          <Button size='small' startIcon={<CommentIcon />}>
+            15
+          </Button>
+          <Button size='small' startIcon={<AttachmentIcon />}>
+            20
+          </Button>
+        </CardActions>
+      </MuiCard>
+    </>
+  )
+}
+
+export default Card
---
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -0,0 +1,41 @@
+import Box from '@mui/material/Box'
+import Card from './Card/Card'
+
+function ListCards() {
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 1,
+          p: '0 5px',
+          m: '0 5px',
+          overflowX: 'hidden',
+          overflowY: 'auto',
+          maxHeight: (theme) =>
+            `calc(${theme.trello.boardContentHeight} -
+                  ${theme.spacing(5)} -
+                  ${theme.trello.columnHeaderHeight} -
+                  ${theme.trello.columnFooterHeight}
+                )`,
+          '&::-webkit-scrollbar-thumb': {
+            backgroundColor: '#ced0da',
+            borderRadius: '8px'
+          },
+          '&::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#bfc2cf',
+            borderRadius: '8px'
+          }
+        }}
+      >
+        <Card />
+        <Card temporaryHideMedia />
+        <Card temporaryHideMedia />
+        <Card temporaryHideMedia />
+      </Box>
+    </>
+  )
+}
+
+export default ListCards
